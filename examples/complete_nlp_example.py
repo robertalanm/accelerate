6b2532b293eea4132a68606e9460e5f5f214f8ce
Carro <robertmyers@hey.com>
2022-11-10 16:55:47 -0600

    fix text loader

--- examples/complete_nlp_example.py
+++ examples/complete_nlp_example.py
@@ -71,8 +71,8 @@ def create_genesis_dataset(config):
     datasets_args = {}
     datasets_args["keep_linebreaks"] = True
     for file_path in file_paths:
-        data = load_dataset("text", data_files=file_path, **datasets_args)
-        pdb.set_trace()
+        data = load_dataset("text", data_files=file_path, **datasets_args)['train']
+        # pdb.set_trace()
         datasets_list.append(data)
     # pdb.set_trace()
     dataset = interleave_datasets(datasets_list)
