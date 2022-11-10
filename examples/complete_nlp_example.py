2b1d55340e2b3015d64982cf623aefd0d5861b61
Carro <robertmyers@hey.com>
2022-11-10 16:52:48 -0600

    pdb debugging

--- examples/complete_nlp_example.py
+++ examples/complete_nlp_example.py
@@ -71,7 +71,7 @@ def create_genesis_dataset(config):
     datasets_args = {}
     datasets_args["keep_linebreaks"] = True
     for file_path in file_paths:
-        datasets_list.append(load_dataset("text", data_files=file_path, **datasets_args))
+        datasets_list.append(load_dataset("text", data_files=file_path, **datasets_args)['text'])
     pdb.set_trace()
     dataset = interleave_datasets(datasets_list)
 
